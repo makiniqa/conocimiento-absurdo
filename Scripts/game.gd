8df82e08717b00b4cb90be1a0bbf920931ae85ed
makiniqa <makiniqa@sutty.nl>
2026-02-23 17:25:02 -0300

    fix: no aparece jugador en menú principal

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -7,6 +7,8 @@ func _ready():
 
 func _on_level_loader_menu_enter() -> void:
 	$Player/Camera2D.enabled = false
+	$Player.visible = false
 
 func _on_level_loader_menu_exit() -> void:
 	$Player/Camera2D.enabled = true
+	$Player.visible = true
