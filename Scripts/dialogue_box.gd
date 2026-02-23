93129b692143481ced1708981464c8e23243788a
Paloma Guevara <guevarapaloma99@gmail.com>
2026-02-23 19:39:40 -0300

    cutsceeeeeeeeeenes

--- Scripts/dialogue_box.gd
+++ Scripts/dialogue_box.gd
@@ -1,8 +1,17 @@
 extends Control
 
+class TextString:
+	var text: String = ""
+	var time: float = 0.0
+	
+	func _init(text: String, time: float):
+		self.text = text
+		self.time = time
+
 var t0: float = 0
 var t1: float = 0
 var displayTime: float = 0
+var displayTextQueue: Array[TextString] = []
 var displayText: String = ""
 var currentText: String = ""
 var textIndex := 0
@@ -14,18 +23,25 @@ signal newCharInText
 func _ready():
 	$CanvasLayer.visible = false
 
-func display_text(text:String, time:float):
-	shouldDisplay = not text.is_empty() and time > 0.0
-	displayTime = time
-	displayText = text
-	currentText = ""
-	textIndex = 0
-	charsPerSecond = text.length() / (time * 0.5)
-	t0 = Time.get_ticks_msec()*1.0e-3
-	$CanvasLayer.visible = true
-	$CanvasLayer/Label.text = currentText
+func queue_display_text(text:String, time: float):
+	displayTextQueue.append(TextString.new(text,time))
 
 func _process(_delta: float) -> void:
+	if not shouldDisplay:
+		var next = displayTextQueue.pop_front()
+		if next == null:
+			return
+		var text = next.text
+		var time = next.time
+		shouldDisplay = not text.is_empty() and time > 0.0
+		displayTime = time
+		displayText = text
+		currentText = ""
+		textIndex = 0
+		charsPerSecond = text.length() / (time * 0.5)
+		t0 = Time.get_ticks_msec()*1.0e-3
+		$CanvasLayer.visible = true
+		$CanvasLayer/Label.text = currentText
 	if shouldDisplay:
 		t1 = Time.get_ticks_msec()*1.0e-3
 		var delta = t1-t0
--- Scripts/nivel_1.gd
+++ Scripts/nivel_1.gd
@@ -3,14 +3,23 @@ extends Level
 var door_entered := false
 var time_door_entered: int
 
+func _ready():
+	$AnimationPlayer.play("enter_level")
+
 func _on_animated_gate_entered(where: Door) -> void:
 	if not door_entered:
 		door_entered = true
-		$DialogueBox.display_text("*salis por la puerta uwu*", 3.0)
+		$DialogueBox.queue_display_text("*salis por la puerta uwu*", 3.0)
 		time_door_entered = Time.get_ticks_msec()
-
 
 func _process(_delta: float) -> void:
 	if door_entered and Time.get_ticks_msec() - time_door_entered >= 2000:
 		change_level = true
 		next_level = 0
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "enter_level":
+		$DialogueBox.queue_display_text("holiii, estoy en busqueda de dios OwO", 5.0)
+		$DialogueBox.queue_display_text("me dijeron que vive acá owo pero no se que onda", 5.0)
+		$DialogueBox.queue_display_text("es re raro este lugar...", 5.0)
+		
