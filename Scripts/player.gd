4666aedea010f7a92a40bf85a729b2a9b331a17c
makiniqa <makiniqa@sutty.nl>
2026-02-23 16:23:30 -0300

    movimiento

---
+++ Scripts/player.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_vector("left", "right", "up", "down")
+	if direction:
+		velocity = direction * SPEED
+	else:
+		velocity = Vector2.ZERO
+
+	move_and_slide()
