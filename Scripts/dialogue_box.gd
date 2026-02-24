7353cca28da737e10ba69f66123cf70d9485b028
Paloma Guevara <guevarapaloma99@gmail.com>
2026-02-23 23:02:58 -0300

    mejore el dialogue box y agregue cositas a la cutscene y al nivel 1 uwu

--- Scripts/dialogue_box.gd
+++ Scripts/dialogue_box.gd
@@ -1,30 +1,37 @@
 extends Control
 
-class TextString:
+var voices = {
+	"default": preload("uid://w0c0yf3det61")
+}
+
+class DialogueComponent:
 	var text: String = ""
 	var time: float = 0.0
+	var voice: String = "default"
 	
-	func _init(text: String, time: float):
+	func _init(text: String, time: float, voice: String = "default"):
 		self.text = text
 		self.time = time
+		self.voice = voice
 
 var t0: float = 0
 var t1: float = 0
 var displayTime: float = 0
-var displayTextQueue: Array[TextString] = []
+var displayTextQueue: Array[DialogueComponent] = []
 var displayText: String = ""
 var currentText: String = ""
 var textIndex := 0
 var shouldDisplay := false
 var charsPerSecond := 0.0
 
-signal newCharInText
+#signal newCharInText
+signal endQueue
 
 func _ready():
 	$CanvasLayer.visible = false
 
-func queue_display_text(text:String, time: float):
-	displayTextQueue.append(TextString.new(text,time))
+func queue_display_text(text:String, time: float, voice: String = "default"):
+	displayTextQueue.append(DialogueComponent.new(text,time,voice))
 
 func _process(_delta: float) -> void:
 	if not shouldDisplay:
@@ -33,24 +40,36 @@ func _process(_delta: float) -> void:
 			return
 		var text = next.text
 		var time = next.time
-		shouldDisplay = not text.is_empty() and time > 0.0
-		displayTime = time
-		displayText = text
-		currentText = ""
-		textIndex = 0
-		charsPerSecond = text.length() / (time * 0.5)
-		t0 = Time.get_ticks_msec()*1.0e-3
-		$CanvasLayer.visible = true
-		$CanvasLayer/Label.text = currentText
+		var voice = next.voice
+		if not text.is_empty() and time > 0.0:
+			shouldDisplay = true
+			displayTime = time
+			displayText = text
+			currentText = ""
+			textIndex = 0
+			charsPerSecond = text.length() / (time * 0.5)
+			t0 = Time.get_ticks_msec()*1.0e-3
+			$CanvasLayer.visible = true
+			$CanvasLayer/Label.text = currentText
+			if voice in voices.keys():
+				$AudioStreamPlayer.stream = voices[voice]
+			else:
+				$AudioStreamPlayer.stream = voices["default"]
 	if shouldDisplay:
 		t1 = Time.get_ticks_msec()*1.0e-3
 		var delta = t1-t0
-		if delta >= textIndex/charsPerSecond and currentText != displayText:
+		var pressed_cancel = Input.is_action_just_pressed("cancel")
+		if pressed_cancel:
+			textIndex = displayText.length()
+			delta = displayTime+0.1
+		elif delta >= textIndex/charsPerSecond and currentText != displayText:
 			textIndex += 1
 			currentText = displayText.substr(0,textIndex)
 			$CanvasLayer/Label.text = currentText
-			newCharInText.emit()
 			$AudioStreamPlayer.play()
+			#newCharInText.emit()
 		if (delta > displayTime):
 			shouldDisplay = false
 			$CanvasLayer.visible = false
+			if displayTextQueue.is_empty():
+				endQueue.emit()
--- Scripts/nivel_1.gd
+++ Scripts/nivel_1.gd
@@ -10,10 +10,13 @@ func _on_animated_gate_entered(where: Door) -> void:
 	if not door_entered:
 		door_entered = true
 		$DialogueBox.queue_display_text("*salis por la puerta uwu*", 3.0)
+		$AnimationPlayer.play("fade_out")
+		$Player.active = false
 		time_door_entered = Time.get_ticks_msec()
+		
 
 func _process(_delta: float) -> void:
-	if door_entered and Time.get_ticks_msec() - time_door_entered >= 2000:
+	if door_entered and not $AnimationPlayer.is_playing():
 		change_level = true
 		next_level = 0
 
@@ -22,4 +25,7 @@ func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 		$DialogueBox.queue_display_text("holiii, estoy en busqueda de dios OwO", 5.0)
 		$DialogueBox.queue_display_text("me dijeron que vive acá owo pero no se que onda", 5.0)
 		$DialogueBox.queue_display_text("es re raro este lugar...", 5.0)
-		
+
+
+func _on_dialogue_box_end_queue() -> void:
+	$Player.active = true
