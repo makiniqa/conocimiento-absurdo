d2b0a3ca525475228578973be4c08869f89b7191
makiniqa <makiniqa@sutty.nl>
2026-02-24 11:29:22 -0300

    player sprite mira para distintos lados

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,8 +4,10 @@ class_name Player
 @export
 var active := true
 
+var direction: = Vector2.DOWN
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+@onready var animation_tree: AnimationTree = $AnimationTree
 
 
 func _physics_process(delta: float) -> void:
@@ -17,9 +19,12 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_vector("left", "right", "up", "down")
+	direction = Input.get_vector("left", "right", "up", "down")
 	if direction:
 		velocity = direction * SPEED
+		var direction_flipped = Vector2(direction.x, -direction.y)
+		animation_tree.set("parameters/StateMachine/IdleState/blend_position", direction_flipped)
+		animation_tree.set("parameters/StateMachine/MoveState/blend_position", direction_flipped)
 	else:
 		velocity = Vector2.ZERO
 
